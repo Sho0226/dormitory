673889ded66a4d4ba75a2b884fb852994d61acee
Akira <s1f102202864@iniad.org>
2024-09-10 18:29:53 +0900

    間違えてruleにflameの内容書いてた

--- client/pages/index.page.tsx
+++ client/pages/index.page.tsx
@@ -6,20 +6,7 @@ const Home = () => {
     <Layout
       render={() => (
         <div className={styles.container}>
-          {/* 既存のコンテンツ */}
-          <div className={styles.box1}>タイトル</div>
-          <div className={styles.box2}>おしらせ</div>
 
-          {/* 枠2の下に左右の枠を配置 */}
-          <div className={styles.sidebarContainer}>
-            <div className={styles.sidebar}>予約</div>
-
-            <div className={styles.sidebarRight}>ルール</div>
-          </div>
-        </div>
-      )}
-    />
-  );
 };
 
 export default Home;
