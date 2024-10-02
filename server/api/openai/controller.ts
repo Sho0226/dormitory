ca1a62874f26219efd71cd95eeb478b30ac2ce1a
Sho0226 <s1F102201616@iniad.org>
2024-10-02 15:59:34 +0900

    fix:openai

--- server/api/openai/controller.ts
+++ server/api/openai/controller.ts
@@ -1,20 +1,20 @@
-import { streamChatCompletion } from '../../../server/domain/openai/event/openaiEvent';
-import { defineController } from './$relay';
+// import { streamChatCompletion } from '../../../server/domain/openai/event/openaiEvent';
+// import { defineController } from './$relay';
 
-export default defineController(() => ({
-  post: async () => {
-    try {
-      const responseText = await streamChatCompletion();
+// export default defineController(() => ({
+//   post: async () => {
+//     try {
+//       const responseText = await streamChatCompletion();
 
-      return {
-        status: 200,
-        body: { response: responseText },
-      };
-    } catch (error) {
-      return {
-        status: 500,
-        body: { error: 'Internal Server Error' },
-      };
-    }
-  },
-}));
+//       return {
+//         status: 200,
+//         body: { response: responseText },
+//       };
+//     } catch (error) {
+//       return {
+//         status: 500,
+//         body: { error: 'Internal Server Error' },
+//       };
+//     }
+//   },
+// }));
--- server/service/openai.ts
+++ server/service/openai.ts
@@ -1,7 +1,7 @@
-import OpenAI from 'openai';
-import { OPENAI_BASE_URL, OPENAI_KEY } from './envValues';
+// import OpenAI from 'openai';
+// import { OPENAI_BASE_URL, OPENAI_KEY } from './envValues';
 
-export const openai = new OpenAI({
-  baseURL: OPENAI_BASE_URL,
-  apiKey: OPENAI_KEY,
-});
+// export const openai = new OpenAI({
+//   baseURL: OPENAI_BASE_URL,
+//   apiKey: OPENAI_KEY,
+// });
