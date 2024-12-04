73aeb34f158b6b9580b2c63c36a5ea2504d36c49
sho0226 <s1f102201616@iniad.org>
2024-12-04 14:49:52 +0900

    add :en from ja

--- server/domain/openai/event/openaiEvent.ts
+++ server/domain/openai/event/openaiEvent.ts
@@ -1,17 +1,20 @@
+import { backOff } from 'exponential-backoff';
 import { openai } from 'service/openai';
 
 // 翻訳用関数
 export async function translateText(text: string, targetLanguage: string = 'en'): Promise<string> {
   let translatedText = '';
 
-  const stream = await openai.chat.completions.create({
-    model: 'gpt-4o-mini',
-    stream: true,
-    messages: [
-      { role: 'system', content: 'You are a helpful assistant and translator.' },
-      { role: 'user', content: `Translate the following text to ${targetLanguage}: "${text}"` },
-    ],
-  });
+  const stream = await backOff(() =>
+    openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      stream: true,
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant and translator.' },
+        { role: 'user', content: ` ${targetLanguage}: "${text}"` },
+      ],
+    }),
+  );
 
   for await (const chunk of stream) {
     translatedText += chunk.choices[0]?.delta?.content || '';
